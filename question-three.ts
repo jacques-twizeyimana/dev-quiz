a0903fbf93fc30ac1de32158e3679788239e83ea
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-03-29 22:22:36 +0200

    feat: finalize question 3

--- question-three.ts
+++ question-three.ts
@@ -1,35 +1,17 @@
-let fudge = 0,
-  recipe = 'name',
-  SPANISH_FUDGE = 80,
-  FRENCH_FUDGE,
-  ENGLISH_FUDGE,
-  amt,
-  base = 1,
-  sugar,
-  chocolate;
-  
+fudge =
+  recipe === 'SPANISH'
+    ? SPANISH_FUDGE
+    : recipe === 'FRENCH'
+    ? FRENCH_FUDGE
+    : recipe == 'ENGLISH'
+    ? ENGLISH_FUDGE
+    : 1;
 
-  
-  fudge = (recipe == 'SPANISH'? SPANISH_FUDGE : recipe == 'FRENCH' ? FRENCH_FUDGE : recipe == 'ENGLISH'? ENGLISH_FUDGE:1)
-
-if (recipe == 'SPANISH') {
-  amt = base * SPANISH_FUDGE;
+const amt = base * fudge;
+// if recipe is SPANISH or FRENCH or ENGLISH
+if (['SPANISH', 'FRENCH', 'ENGLISH'].includes(recipe)) {
   sugar = 2 * bottom(amt) + top_(amt) * 1.17;
-
-} else if (recipe == 'FRENCH' || recipe == 'ENGLISH') {
-  amt = base * fudge;
-  sugar = 2 * bottom(amt) + top_(amt) * 1.17;
-  if (recipe == 'FRENCH') {
-    chocolate = 7;
-  }
+  if (recipe === 'FRENCH') chocolate = 7;
 } else {
-  amt = base;
-  sugar = 2 * bottom(amt) + top_(amt) * 1.17;
-}
-
-function bottom(num: any) {
-  return 0;
-}
-function top_(num: any) {
-  return 0;
+  sugar = 2 * bottom(base) + top_(base) * 1.17;
 }
