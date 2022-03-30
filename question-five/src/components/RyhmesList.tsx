73e7292c6dbf81adecf0ca4fffcbb8878697e7f8
jacques-twizeyimana <sandbergjacques500@gmail.com>
2022-03-30 12:39:17 +0200

    chore: change loder gif

--- question-five/src/components/RyhmesList.tsx
+++ question-five/src/components/RyhmesList.tsx
@@ -10,7 +10,10 @@ export default function RyhmesList({ ryhmes, isLoading, word }: IProps) {
   return (
     <div className="py-2">
       {isLoading ? (
-        <img src="/rolling.gif" className="block object-cover h-8" />
+        <img
+          src="https://c.tenor.com/I6kN-6X7nhAAAAAi/loading-buffering.gif"
+          className="block object-cover max-w-full h-32"
+        />
       ) : word.length === 0 ? (
         <h4 className="text-gray-400 text-xl border border-gray-300 border-dashed py-10 px-6 rounded">
           Found ryhmes will appear here
